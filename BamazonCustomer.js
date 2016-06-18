edc63e67e46866d3dc6fe5c462915ce1e04d5c36
Ednas <edna@edna.tech>
2016-06-18 11:50:41 -0400

    got the Select * From statement to work

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,12 +1,5 @@
-// First Display All of the Items available for sale. This initial display, should include the ids, names, and prices of products for sale
-
-// Users should then be prompted with two messages. The first message should ask them the ID of the product they would like to buy. The second message should ask them how many of the product they would like to buy.
-
-// Once the customer has placed the order: Your application should...
-
-// Check if your store has enough quantity of the product to meet the customer's request. If not, you should respond to the user by saying: "Insufficient quantity" and prevent the order from going through.
-
-// If your store DOES have enough of the product to meet the customer's request, you should fulfill their order. This means that you should show them the total cost of their puchase. Then update the SQL database to reflect the remaining quantity.
+//Welcome to Bamazon, use this application to find and purchase
+//your favorite Bamazon items
 
 //connects to the Bamazon_db database
 var mysql=require('mysql');
@@ -24,16 +17,20 @@ connection.connect(function(err){
 	console.log();})
 
 //Display All Items available for sale
-connection.query('SELECT * FROM Bamazon_db',function(err,res){
+connection.query('SELECT * FROM Bamazon_db.products;',function(err,res){
 	for(var i=0;i<res.length;i++){
-		console.log(res[i].ItemID);
-			//+" | "+res[i].ProductName+" | "+res[i].Price+" | "+res[i].StockQuantity);
+		console.log(res[i].ItemID+" | "+res[i].ProductName+" | "+res[i].Price+" | "+res[i].StockQuanitiy);
 		}
-	console.log("-----------------------------------");})
+	console.log("-----------------------------------");
+});
 
+//2 messages
 
+//First message = ID of the product they would like to buy
 
-// connection.query('SELECT * FROM Bamazon_db WHERE genre=?',['country'],function(err,res){
-// 	for(var i=0;i<res.length;i++){
-// 		console.log(res[i].ID+" | "+res[i].song+" | "+res[i].artist+" | "+res[i].genre);}})
+//Second message = how many of the product they would like to buy
+
+// Check if your store has enough quantity of the product to meet the customer's request. If not, you should respond to the user by saying: "Insufficient quantity" and prevent the order from going through.
+
+// If your store DOES have enough of the product to meet the customer's request, you should fulfill their order. This means that you should show them the total cost of their puchase. Then update the SQL database to reflect the remaining quantity.
 
